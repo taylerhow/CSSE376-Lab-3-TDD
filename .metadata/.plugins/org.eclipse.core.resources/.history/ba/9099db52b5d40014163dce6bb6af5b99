a8435c5c940a7d757bc8fc3b06b1883f5dc9f805
Tayler How <taylerhow@gmail.com>
2015-03-27 15:40:45 -0400

    Changed name of eclipse project
    renamed PrimeFactors to Lab 3 TDD

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ba/9099db52b5d40014163dce6bb6af5b99
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrimeNumberGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ PrimeNumberGenerator/src/PrimeNumberGenerator.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class PrimeNumberGenerator {
+	
+	public PrimeNumberGenerator(){
+		
+	}
+
+	public List<Integer> generate(Integer inputNumber) {
+		// TODO Auto-generated method stub
+		return Arrays.asList(2);
+	}
+
+}
